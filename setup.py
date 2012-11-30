16628a3f31a91e935fd718f4060a5aa913eb76c5
Thomas Bischof <tbischofd@mit.edu>
2012-11-30 16:23:46 -0500

    Added a setup file for installing the module.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="winspec",
+      version="1.0",
+      author="Thomas Bischof", 
+      author_email="tbischof@mit.edu",
+      packages=["winspec"])
+      
+      
